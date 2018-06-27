c4800914f6c6e2a1512ec191f9eecaab51569df0
Gökcan Değirmenci <gokcan.degirmenci@ug.bilkent.edu.tr>
2018-06-27 14:20:20 +0300

    Handle error and loading situations

--- src/web/index.js
+++ src/web/index.js
@@ -4,23 +4,38 @@
  */
 
 import React, { Component } from 'react'
-// import styles from './styles.css'
+import styles from './styles.css'
 
 type Props = {
   src: string,
+  onError?: (err: string) => void,
+  onLoad?: (image: Image) => void,
+  loadingIndicatorSource?: string,
 }
 
-const placeholder = './placeholder.png'
+type State = {
+  isLoading: boolean,
+  src: string,
+  error?: string | boolean,
+  width: number,
+  height: number
+}
 
-export default class ShimmerImage extends Component<Props> {
+export default class ShimmerImage extends Component<Props, State> {
+  state: State = {
+    isLoading: false,
+    src: '',
+    error: ''
+  };
+
   async componentWillMount() {
     const { src } = this.props
-    this.setState({ isLoading: true, src: placeholder })
+    this.setState({ isLoading: true })
     try {
       const uri = await this.loadImage(src)
       this.setState({ isLoading: false, src: uri })
     } catch (error) {
-      this.setState({ error })
+      this.setState({ error, isLoading: false })
     }
   }
 
@@ -29,6 +44,10 @@ export default class ShimmerImage extends Component<Props> {
       const img = new Image()
       img.src = uri
       img.onload = () => {
+        this.setState({ width: img.width, height: img.height })
+        if (this.props.onLoad) {
+          this.props.onLoad(img)
+        }
         resolve(uri)
       }
       img.onerror = () => {
@@ -38,10 +57,21 @@ export default class ShimmerImage extends Component<Props> {
   }
 
   render() {
-    const { src, error } = this.state
-    const { ...passed } = this.props
-    let passedProps = {...passed, ...{src}}
+    const { src, error, isLoading, width, height } = this.state
+    const { onError, loadingIndicatorSource, ...passed } = this.props
+    let passedProps = { ...passed, ...{ src, width, height } }
 
-    return (!error ? <img {...passedProps} /> : null)
+    if (error) {
+      return onError ? onError(error) : null
+    } else if (isLoading) {
+      if (loadingIndicatorSource) {
+        return <img src={loadingIndicatorSource} />
+      } else {
+        return (<div className={styles.shimmer}>
+          <div className={styles.shimmerbg} /></div>)
+      }
+    } else {
+      return <img {...passedProps} />
+    }
   }
 }
