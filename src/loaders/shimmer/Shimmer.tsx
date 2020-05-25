7f5055f0aff2c7a341424159387fc2a3e2620e5f
Gokcan Degirmenci <gokcande@gmail.com>
2020-05-25 20:14:24 +0300

    Use PropTypes and clsx

--- src/loaders/shimmer/Shimmer.tsx
+++ src/loaders/shimmer/Shimmer.tsx
@@ -1,21 +1,34 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import clsx from 'clsx'
 
 import './styles.css'
 
+const DEFAULT_DURATION_MS = 1600
+const DEFAULT_HEIGHT = 400
+const DEFAULT_WIDTH = 400
 export interface ShimmerProps {
   height: number
   width: number
+  className?: string
   duration?: number
 }
 
-const calcShimmerStyle = (width: number, height: number, duration = 1600) => ({
+const calcShimmerStyle = (width: number, height: number, duration = DEFAULT_DURATION_MS) => ({
   backgroundSize: `${width * 10}px ${height}px`,
   animationDuration: `${(duration / 1000).toFixed(1)}s`
 })
 
-export const Shimmer = ({ duration, height = 400, width = 400 }: ShimmerProps) => {
+export const Shimmer = ({ className, duration, height = DEFAULT_HEIGHT, width = DEFAULT_WIDTH }: ShimmerProps) => {
   const shimmerStyle = calcShimmerStyle(width, height, duration)
   const style = { ...shimmerStyle, ...{ height, width } }
 
-  return <span className='shimmer' style={style} />
+  return <span className={clsx('shimmer', className)} style={style} />
+}
+
+Shimmer.propTypes = {
+  height: PropTypes.number.isRequired,
+  width: PropTypes.number.isRequired,
+  className: PropTypes.string,
+  duration: PropTypes.number
 }
