ecbd1cd5561307f13c0d696110c51dc50333db51
azhry <arliansyah_azhary@yahoo.com>
2020-07-06 14:38:33 +0700

    added shimmerColor props for customizing shimmer color and percentage support for shimmer width

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
-import Image, { Shimmer } from 'react-shimmer'
+import Image, { Shimmer } from 'react-shimmer';
 
 export default function App() {
   return (
     <div>
+      <div style={{ width: 400 }}>
+        <Image
+          src='https://source.unsplash.com/random/400x300'
+          fallback={(
+            <Shimmer 
+            shimmerColor="linear-gradient(to right, red 8%, rgb(222, 222, 222) 18%,  red 33%)"
+              width="100%" 
+              height={300}/>
+          )}/>
+      </div>
+      
       <Image
-        src='https://source.unsplash.com/random/800x600'
-        fallback={<Shimmer width={800} height={600} />}
-      />
+        src='https://source.unsplash.com/random/400x300'
+        fallback={(
+          <Shimmer 
+            width={400} 
+            height={300}/>
+        )}/>
     </div>
   )
 }
--- src/loaders/shimmer/Shimmer.tsx
+++ src/loaders/shimmer/Shimmer.tsx
@@ -7,29 +7,56 @@ import './styles.css'
 const DEFAULT_DURATION_MS = 1600
 const DEFAULT_HEIGHT = 400
 const DEFAULT_WIDTH = 400
+const DEFAULT_SHIMMER_COLOR = 'linear-gradient(to right, rgb(238, 238, 238) 8%, rgb(222, 222, 222) 18%,  rgb(238, 238, 238) 33%)'
 
 export interface ShimmerProps {
-  height: number
-  width: number
+  height: number | string
+  width: number | string
   className?: string
   duration?: number
+  shimmerColor?: string
 }
 
-const calcShimmerStyle = (width: number, height: number, duration = DEFAULT_DURATION_MS) => ({
-  backgroundSize: `${width * 10}px ${height}px`,
-  animationDuration: `${(duration / 1000).toFixed(1)}s`
-})
+const calcShimmerStyle = (width: number | string, height: number | string, duration = DEFAULT_DURATION_MS) => {
+  let backgroundWidth = '0';
+  if (typeof width == 'string' && width != null) {
+    const number = width.match(/\d+/g) || [];
+    if (number.length > 0) {
+      const value = Number(number);
+      backgroundWidth = `${value * 10}%`;
+    }
+  }
+  else {
+    backgroundWidth = `${width * 10}px`;
+  }
 
-export const Shimmer = ({ className, duration, height = DEFAULT_HEIGHT, width = DEFAULT_WIDTH }: ShimmerProps) => {
+  return {
+    backgroundSize: `${backgroundWidth} ${height}px`,
+    animationDuration: `${(duration / 1000).toFixed(1)}s`
+  }
+}
+
+export const Shimmer = ({ className, duration, height = DEFAULT_HEIGHT, width = DEFAULT_WIDTH, shimmerColor = DEFAULT_SHIMMER_COLOR }: ShimmerProps) => {
   const shimmerStyle = calcShimmerStyle(width, height, duration)
-  const style = { ...shimmerStyle, ...{ height, width } }
+  const style = { ...shimmerStyle, ...{ 
+    height, 
+    width,
+    backgroundImage: shimmerColor
+  } }
 
   return <div className={clsx('shimmer', className)} style={style} />
 }
 
 Shimmer.propTypes = {
-  height: PropTypes.number.isRequired,
-  width: PropTypes.number.isRequired,
+  height: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.string
+  ]).isRequired,
+  width: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.string
+  ]).isRequired,
   className: PropTypes.string,
-  duration: PropTypes.number
+  duration: PropTypes.number,
+  shimmerColor: PropTypes.string
 }
